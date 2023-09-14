f8a0b38d45ad423cac84e4169061be4de334095d
j0nmi <jmiguelmag@educacion.navarra.es>
2023-09-14 08:21:56 +0200

    Subida 1 KarolisT

---
+++ Examen/ExamenSolution/Domain/Models/Persona.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Domain.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+    }
+}
